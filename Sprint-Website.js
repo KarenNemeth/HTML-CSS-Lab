b5802d71d77b32cc27fc7f49d77b573198505fca
Karen Nemeth <karen61990@gmail.com>
2016-09-19 18:29:34 +0200

    added modal JSON files, no POST support

--- Sprint-Website.js
+++ Sprint-Website.js
@@ -5,7 +5,14 @@ $(document).ready(function() {
     });
     $('#submitMailingList').on("click keypress", function(e){
         if (e.which === 13 || e.type === 'click') {
-            $.post('Sprint-Website-Mailing-List.json');
+            var firstName = $('#firstName').val()
+            var lastName = $('#lastName').val()
+            var email = $('#email').val()
+            // $.post('Sprint-Website-Mailing-List.json', { FirstName: $('#firstName').val(), LastName: $('#lastName').val(), Email: $('#email').val() })
+            //     .done(function(data) {
+            //         console.log(data);
+            //     });
+            $('#mailingListContainer').empty().html("We're sorry " + firstName + " " + lastName + " but the mailing list is still under construction while we learn .POST requests! \ Therefore we will be unable to email you at " + email + ".");
         }
     });
 
